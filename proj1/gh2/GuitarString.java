9a2a780bcfbf6f1d64446f67033f5fd23cb155ba
Pius Lau Jueng <piusssss0405@gmail.com>
2024-06-05 18:29:25 +0800

    Fix minor changes

--- proj1/gh2/GuitarString.java
+++ proj1/gh2/GuitarString.java
@@ -1,10 +1,9 @@
 package gh2;
+
+
 import deque.Deque;
 import deque.LinkedListDeque;
 
-// TODO: uncomment the following import once you're ready to start this portion
-// import deque.Deque;
-// TODO: maybe more imports
 
 //Note: This file will not compile until you complete the Deque implementations
 public class GuitarString {
@@ -15,15 +14,10 @@ public class GuitarString {
     private static final double DECAY = .996; // energy decay factor
 
     /* Buffer for storing sound data. */
-    // TODO: uncomment the following line once you're ready to start this portion
     private Deque<Double> buffer;
 
     /* Create a guitar string of the given frequency.  */
     public GuitarString(double frequency) {
-        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
-        //       cast the result of this division operation into an int. For
-        //       better accuracy, use the Math.round() function before casting.
-        //       Your should initially fill your buffer array with zeros.
         /*Selected LinkedListDeque as Deque for buffer to enhance accuracy of size of buffer*/
         /*Using ArrayDeque might result in different initial capacity*/
         buffer = new LinkedListDeque<>();
@@ -36,10 +30,6 @@ public class GuitarString {
 
     /* Pluck the guitar string by replacing the buffer with white noise. */
     public void pluck() {
-        // TODO: Dequeue everything in buffer, and replace with random numbers
-        //       between -0.5 and 0.5. You can get such a number by using:
-        //       double r = Math.random() - 0.5;
-        //
         //       Make sure that your random numbers are different from each
         //       other. This does not mean that you need to check that the numbers
         //       are different from each other. It means you should repeatedly call
@@ -56,21 +46,14 @@ public class GuitarString {
      * the Karplus-Strong algorithm.
      */
     public void tic() {
-        // TODO: Dequeue the front sample and enqueue a new sample that is
-        //       the average of the two multiplied by the DECAY factor.
-        //       **Do not call StdAudio.play().**
-        while (!buffer.isEmpty()) {
-            double frontSample = buffer.removeFirst();
-            double newSample = (frontSample + sample())*DECAY;
-            buffer.addLast(newSample);
-            StdAudio.play(newSample);
-        }
+        double frontSample = buffer.removeFirst();
+        double newSample = ((frontSample + sample())*0.5)*DECAY;
+        buffer.addLast(newSample);
     }
 
     /* Return the double at the front of the buffer. */
     public double sample() {
-        // TODO: Return the correct thing.
         return buffer.get(0);
     }
 }
-    // TODO: Remove all comments that say TODO when you're done.
+
